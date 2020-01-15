c9c5fbf76bcfb1b6c62ef383fe6c77d03219287b
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2020-01-15 00:06:39 -0300

    add put and delete route

--- backend/src/controllers/DevController.ts
+++ backend/src/controllers/DevController.ts
@@ -35,3 +35,44 @@ export async function store(request: Request, response: Response) {
 
   return response.json(dev);
 }
+
+export async function update(request: Request, response: Response) {
+  const { github_username } = request.params;
+
+  let dev: any = await DevSchema.findOne({ github_username });
+
+  if (!dev) {
+    return response.status(400).json({ message: "Usuário não encontrado!" });
+  }
+
+  const {
+    name = dev.name,
+    bio = dev.bio,
+    longitude = dev.location.coordinates[0],
+    latitude = dev.location.coordinates[1],
+    avatar_url = dev.avatar_url
+  } = request.body;
+
+  const techs = request.body.techs ? parseStringAsArray(request.body.techs) : dev.techs;
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+  let updatedDev = await DevSchema.findOneAndUpdate({ github_username }, { name, techs, bio, avatar_url, location }, { new: true });
+
+  return response.json(updatedDev);
+}
+
+export async function destroy(request: Request, response: Response) {
+  const { github_username } = request.params;
+  let dev = await DevSchema.findOne({ github_username });
+
+  if (!dev) {
+    return response.status(400).json({ message: "Usuário não encontrado!" });
+  }
+
+  await DevSchema.findOneAndDelete({ github_username });
+  return response.json({ deleted: true });
+}
